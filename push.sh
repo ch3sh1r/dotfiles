d0b85f39efd82ae330875d3cea280e59fe0e4c05
Alexey Bednyakov <ch3sh1r@ya.ru>
2013-06-19 16:04:26 +0400

    Push script updated for metasploit.

--- push.sh
+++ push.sh
@@ -4,14 +4,13 @@
 cd "$(dirname "$0")"
 if [ $1 ] ; then
     case $1 in
-        -f)
-            temp="/tmp/dotfiles-"`date | md5sum | cut -f1 -d' ' | head -c10`
-            mkdir $temp
-            rsync --exclude ".git/" \
-                  --exclude ".DS_Store" \
+        -f) #for server-like shell
+            temp="$(mktemp -t ".dfXXXXXXX" -d)"
+            rsync --exclude ".*" \
                   --exclude "push.sh" \
                   --exclude "config" \
-                  --exclude "README.markdown" \
+                  --exclude "msf4" \
+                  --exclude "README.md" \
                   -av . $temp > /dev/null
             for dotfile in `ls $temp`; do
                 mv -f $temp"/"$dotfile $temp"/."$dotfile
@@ -19,22 +18,37 @@ if [ $1 ] ; then
             rsync -av $temp"/." ${HOME} > /dev/null
             rm -rf $temp
         ;;
-        -l)
+        -l) #for workstation
             for dotfile in `ls`
             do
-                if [ $dotfile == "push.sh" -o $dotfile == "README.markdown" ]
-                then
+                if [ $dotfile == "push.sh" -o \
+                     $dotfile == "README.md" ]; then
                     false
-                elif [ $dotfile == "config" ]
-                then
-                    for config in `ls config`
-                    do
-                        rm -rf ${HOME}"/.config/"$config
-                        ln -s ${PWD}"/config/"$config ${HOME}"/.config/"$config
+
+                elif [ $dotfile == "config" ]; then
+                    if ! [ -d "${HOME}/.config" ]; then 
+                        mkdir "${HOME}/.config" 
+                    fi
+                    for config in `ls config`; do
+                        rm -rf "${HOME}/.config/${config}"
+                        ln -s "${PWD}/config/${config}" "${HOME}/.config/${config}"
                     done
+
+                elif [ $dotfile == "msf4" ]; then
+                    msftemp="$(mktemp --tmpdir="${HOME}" -t ".msf4XXXXXXX" -d)"
+                    for msffile in history local logs loot; do
+                        [ -f "${HOME}/.msf4/${msffile}" ] && \
+                            mv "${HOME}/.msf4/${msffile}" ${msftemp}; done
+                    rm -rf "${HOME}/.msf4"
+                    ln -s "${PWD}/msf4" "${HOME}/.$dotfile"
+                    for msffile in history local logs loot; do
+                        [ -f "${msftemp}/${msffile}" ] && \
+                            mv "${msftemp}/${msffile}" "${HOME}/.msf4"; done
+                    rm -rf ${msftemp}
+
                 else
-                    rm -rf ${HOME}"/."$dotfile
-                    ln -s ${PWD}"/"$dotfile ${HOME}"/."$dotfile
+                    rm -rf "${HOME}/.${dotfile}"
+                    ln -s "${PWD}/$dotfile" "${HOME}/.$dotfile"
                 fi
             done
         ;;
@@ -44,4 +58,3 @@ else
     echo "  push.sh -f  - change existing."
     echo "  push.sh -l  - remove existing and create symlinks."       
 fi
-
