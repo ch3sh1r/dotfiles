badb74628ee1e923d72569ef7140719c929b7e3c
Alexey Bednyakov <ch3sh1r@ya.ru>
2013-09-19 17:23:47 +0400

    Ubuntu colors to standart zenburn theme.

--- config/awesome/themes/zenburn/theme.lua
+++ config/awesome/themes/zenburn/theme.lua
@@ -15,74 +15,74 @@ theme.wallpaper = "/home/ch3sh1r/.config/awesome/themes/zenburn/zenburn-backgrou
 theme.font      = "ubuntu 8"
 
 -- {{{ Colors
-theme.fg_normal  = "#DCDCCC"
-theme.fg_focus   = "#F0DFAF"
-theme.fg_urgent  = "#CC9393"
-theme.bg_normal  = "#3F3F3F"
-theme.bg_focus   = "#1E2320"
-theme.bg_urgent  = "#3F3F3F"
+theme.fg_normal  = "#dcdccc"
+theme.fg_focus   = "#f0dfaf"
+theme.fg_urgent  = "#cc9393"
+theme.bg_normal  = "#3c3b37"
+theme.bg_focus   = "#1e2320"
+theme.bg_urgent  = "#3f3f3f"
 theme.bg_systray = theme.bg_normal
 -- }}}
 
--- {{{ Borders
+-- {{{ borders
 theme.border_width  = 2
-theme.border_normal = "#3F3F3F"
-theme.border_focus  = "#6F6F6F"
-theme.border_marked = "#CC9393"
+theme.border_normal = "#3f3f3f"
+theme.border_focus  = "#6f6f6f"
+theme.border_marked = "#cc9393"
 -- }}}
 
--- {{{ Titlebars
-theme.titlebar_bg_focus  = "#3F3F3F"
-theme.titlebar_bg_normal = "#3F3F3F"
+-- {{{ titlebars
+theme.titlebar_bg_focus  = "#3f3f3f"
+theme.titlebar_bg_normal = "#3f3f3f"
 -- }}}
 
--- There are other variable sets
+-- there are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
 -- [taglist|tasklist]_[bg|fg]_[focus|urgent]
 -- titlebar_[normal|focus]
 -- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
--- Example:
---theme.taglist_bg_focus = "#CC9393"
+-- example:
+--theme.taglist_bg_focus = "#cc9393"
 -- }}}
 
--- {{{ Widgets
--- You can add as many variables as
+-- {{{ widgets
+-- you can add as many variables as
 -- you wish and access them by using
 -- beautiful.variable in your rc.lua
---theme.fg_widget        = "#AECF96"
---theme.fg_center_widget = "#88A175"
---theme.fg_end_widget    = "#FF5656"
---theme.bg_widget        = "#494B4F"
---theme.border_widget    = "#3F3F3F"
+--theme.fg_widget        = "#aecf96"
+--theme.fg_center_widget = "#88a175"
+--theme.fg_end_widget    = "#ff5656"
+--theme.bg_widget        = "#494b4f"
+--theme.border_widget    = "#3f3f3f"
 -- }}}
 
--- {{{ Mouse finder
-theme.mouse_finder_color = "#CC9393"
+-- {{{ mouse finder
+theme.mouse_finder_color = "#cc9393"
 -- mouse_finder_[timeout|animate_timeout|radius|factor]
 -- }}}
 
--- {{{ Menu
--- Variables set for theming the menu:
+-- {{{ menu
+-- variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
 theme.menu_height = 15
 theme.menu_width  = 100
 -- }}}
 
--- {{{ Icons
--- {{{ Taglist
+-- {{{ icons
+-- {{{ taglist
 theme.taglist_squares_sel   = "/usr/share/awesome/themes/zenburn/taglist/squarefz.png"
 theme.taglist_squares_unsel = "/usr/share/awesome/themes/zenburn/taglist/squarez.png"
 --theme.taglist_squares_resize = "false"
 -- }}}
 
--- {{{ Misc
+-- {{{ misc
 theme.awesome_icon           = "/usr/share/awesome/themes/zenburn/awesome-icon.png"
 theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
 -- }}}
 
--- {{{ Layout
+-- {{{ layout
 theme.layout_tile       = "/usr/share/awesome/themes/zenburn/layouts/tile.png"
 theme.layout_tileleft   = "/usr/share/awesome/themes/zenburn/layouts/tileleft.png"
 theme.layout_tilebottom = "/usr/share/awesome/themes/zenburn/layouts/tilebottom.png"
@@ -97,7 +97,7 @@ theme.layout_magnifier  = "/usr/share/awesome/themes/zenburn/layouts/magnifier.p
 theme.layout_floating   = "/usr/share/awesome/themes/zenburn/layouts/floating.png"
 -- }}}
 
--- {{{ Titlebar
+-- {{{ titlebar
 theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/zenburn/titlebar/close_focus.png"
 theme.titlebar_close_button_normal = "/usr/share/awesome/themes/zenburn/titlebar/close_normal.png"
 
