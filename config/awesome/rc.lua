b2ae7033e14af76f7234f28f90364be962fce90d
Alexey Bednyakov <ch3sh1r@ya.ru>
2013-10-17 16:44:40 +0400

    Shifty implementation init.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -11,6 +11,7 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local vicious = require("vicious")
+local shifty = require("shifty")
 
 -- {{{ Error handling
     -- Check if awesome encountered an error during startup and fell back to
@@ -86,13 +87,135 @@ if beautiful.wallpaper then
 end
 -- }}}
 
--- {{{ Tags
-    -- Define a tag table which hold all screen tags.
-    tags = {}
-    for s = 1, screen.count() do
-        -- Each screen has its own tag table.
-        tags[s] = awful.tag({ 1, 2, 3, 4, 5 }, s, layouts[2])
-    end
+-- {{{ Shifty
+    -- Tags
+    shifty.config.tags = {
+        w1 = {
+            layout    = awful.layout.suit.max,
+            mwfact    = 0.60,
+            exclusive = false,
+            position  = 1,
+            init      = true,
+            screen    = 1,
+            slave     = true,
+        },
+        web = {
+            layout      = awful.layout.suit.tile.bottom,
+            mwfact      = 0.65,
+            exclusive   = true,
+            max_clients = 1,
+            position    = 4,
+            spawn       = browser,
+        },
+        mail = {
+            layout    = awful.layout.suit.tile,
+            mwfact    = 0.55,
+            exclusive = false,
+            position  = 5,
+            spawn     = mail,
+            slave     = true
+        },
+        media = {
+            layout    = awful.layout.suit.float,
+            exclusive = false,
+            position  = 8,
+        },
+        office = {
+            layout   = awful.layout.suit.tile,
+            position = 9,
+        },
+    }
+
+    -- Application matching rules
+    -- Order here matters, early rules will be applied first
+    shifty.config.apps = {
+        {
+            match = {
+                "Navigator",
+                "Vimperator",
+                "Gran Paradiso",
+            },
+            tag = "web",
+        },
+        {
+            match = {
+                "Shredder.*",
+                "Thunderbird",
+                "mutt",
+            },
+            tag = "mail",
+        },
+        {
+            match = {
+                "pcmanfm",
+            },
+            slave = true
+        },
+        {
+            match = {
+                "OpenOffice.*",
+                "Abiword",
+                "Gnumeric",
+            },
+            tag = "office",
+        },
+        {
+            match = {
+                "Mplayer.*",
+                "Mirage",
+                "gimp",
+                "gtkpod",
+                "Ufraw",
+                "easytag",
+            },
+            tag = "media",
+            nopopup = true,
+        },
+        {
+            match = {
+                "MPlayer",
+                "Gnuplot",
+                "galculator",
+            },
+            float = true,
+        },
+        {
+            match = {
+                terminal,
+            },
+            honorsizehints = false,
+            slave = true,
+        },
+        {
+            match = {""},
+            buttons = awful.util.table.join(
+                awful.button({}, 1, function (c) client.focus = c; c:raise() end),
+                awful.button({modkey}, 1, function(c)
+                    client.focus = c
+                    c:raise()
+                    awful.mouse.client.move(c)
+                    end),
+                awful.button({modkey}, 3, awful.mouse.client.resize)
+                )
+        },
+    }
+
+    -- SHIFTY: default tag creation rules
+    -- parameter description
+    --  * floatBars : if floating clients should always have a titlebar
+    --  * guess_name : should shifty try and guess tag names when creating
+    --                 new (unconfigured) tags?
+    --  * guess_position: as above, but for position parameter
+    --  * run : function to exec when shifty creates a new tag
+    --  * all other parameters (e.g. layout, mwfact) follow awesome's tag API
+    shifty.config.defaults = {
+        layout = awful.layout.suit.tile.bottom,
+        ncol = 1,
+        mwfact = 0.60,
+        floatBars = true,
+        guess_name = true,
+        guess_position = true,
+    }
 -- }}}
 
 -- {{{ Wibox
