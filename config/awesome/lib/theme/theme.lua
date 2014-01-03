851d427e0a177f57a3415828b4dea837d30daf48
Alexey Bednyakov <ch3sh1r@ya.ru>
2014-01-03 22:50:25 +0400

    Grumpy theme.

--- config/awesome/lib/theme/theme.lua
+++ config/awesome/lib/theme/theme.lua
@@ -58,7 +58,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = theme_dir .. "/pattern.png"
+theme.wallpaper = theme_dir .. "/grumpy.png"
 
 -- You can use your own layout icons like this:
 theme.layout_floating  = theme_dir .. "/icons/floating.png"
