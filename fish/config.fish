519020240cda609a2c00ed8f08fd6c2c617c4abc
Aleksei Bedniakov <aleksei.bedniakov@veeam.com>
2023-08-01 12:45:11 +0200

    Screening template for fish and 7z extraction

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
+    # Run ssh agent plugin
     fish_ssh_agent
+
+    # Set up session logging
+    #if test -z "$SCRIPTED"
+        #set -gx SCRIPTED nested
+        #set script_share "$HOME/.local/share/fish/scripts"
+        #mkdir -p $script_share
+        #script -q $script_share/(date +%s)
+        #echo WARNING: session is no longer being captured
+    #end
 end
