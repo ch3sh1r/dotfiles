f7ceaebc7bbf3a4dbc14e48f0e5a59dbab06ffdd
Aleksei Bedniakov <aleksei.bedniakov@veeam.com>
2023-10-27 12:50:14 +0200

    New Fish abbreviations format

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,29 @@ if status is-interactive
         #echo WARNING: session is no longer being captured
     #end
 end
+
+# Abbreviations
+abbr -a p 'ps -auxw'
+
+# Git abbreviations
+abbr -a g    'git'
+abbr -a ga   'git add'
+abbr -a gca  'git commit -v -a'
+abbr -a gc   'git commit --verbose'
+abbr -a gcl  'git clone --recursive'
+abbr -a gco  'git checkout'
+abbr -a gd   'git diff'
+abbr -a glg  'git log --stat'
+abbr -a gp   'git push'
+abbr -a grhh 'git reset --hard'
+abbr -a gss  'git status'
+abbr -a gsta 'git stash push'
+abbr -a gstp 'git stash pop'
+abbr -a gup  'git pull'
+
+# Global abbreviations
+abbr -a G --position anywhere               '| grep'
+abbr -a C --position anywhere --set-cursor '%| xclip -selection clipboard'
+abbr -a L --position anywhere --set-cursor '%| less -R'
+abbr -a T --position anywhere --set-cursor '%| tail'
+abbr -a H --position anywhere --set-cursor '%| head'
