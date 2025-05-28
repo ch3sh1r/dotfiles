0ecba5af1d01761ea1b61aa82f7e1f11149c9e53
Aleksei Bedniakov <x@rxtx.wtf>
2025-05-28 12:04:53 +0200

    Typr config

--- nvim/lua/plugins/typr.lua
+++ nvim/lua/plugins/typr.lua
@@ -2,5 +2,14 @@ return {
   "nvzone/typr",
   dependencies = "nvzone/volt",
   opts = {},
+  config = {
+    mode = "phrases",
+    winlayout = "responsive",
+    kblayout = "qwerty",
+    wpm_goal = 90,
+    numbers = true,
+    symbols = false,
+    random = false,
+  },
   cmd = { "Typr", "TyprStats" },
 }
