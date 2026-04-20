594cb747991268d4d47e17bbb46fc1b07b511599
Aleksei Bedniakov <x@rxtx.wtf>
2026-04-20 14:20:26 +0200

    verbose monitoring layout

--- fish/functions/tml.fish
+++ fish/functions/tml.fish
@@ -1,18 +1,34 @@
-function tml --description "Create a Tmux Mullvad Layout: watch, ping, and shell panes"
+function tml --description "Create a Tmux Mullvad Layout: status, logs, wg, relay, ping, and shell panes"
     if test -z "$TMUX"
         echo "You must start tmux to use tml."
         return 1
     end
 
-    set -l current_dir $PWD
     set -l watch_pane $TMUX_PANE
     tmux rename-window -t $watch_pane mullvad
-
     set -l shell_pane (tmux split-window -v -p 10 -t $watch_pane -P -F '#{pane_id}')
-    set -l ping_pane (tmux split-window -h -t $watch_pane -P -F '#{pane_id}')
+    set -l relay_pane (tmux split-window -v -p 30 -t $watch_pane -P -F '#{pane_id}')
+    set -l wg_pane (tmux split-window -h -t $relay_pane -P -F '#{pane_id}')
+    set -l journal_pane (tmux split-window -h -t $watch_pane -P -F '#{pane_id}')
+    set -l ping_pane (tmux split-window -v -t $journal_pane -P -F '#{pane_id}')
 
-    tmux send-keys -t $watch_pane "watch -c 'mullvad status --json | jq -C'" C-m
-    tmux send-keys -t $ping_pane "ping 1.1" C-m
-    tmux send-keys -t $shell_pane "mullvad reconnect"
+    tmux send-keys -t $watch_pane \
+        "watch -c 'mullvad status --json | jq -C'" C-m
+
+    tmux send-keys -t $journal_pane \
+        "journalctl -u mullvad-daemon -f --no-hostname -o cat | grep --line-buffered -iE 'error|warn|connect|handshake|relay|timeout|block|disconn|obfusc'" C-m
+
+    tmux send-keys -t $ping_pane \
+        "ping 1.1.1.1" C-m
+
+    tmux send-keys -t $wg_pane \
+        "watch -n1 'mullvad status -v'" C-m
+
+    tmux send-keys -t $relay_pane \
+        "watch -n1 'mullvad relay get; echo ---; mullvad obfuscation get'" C-m
+
+    tmux send-keys -t $shell_pane \
+        "mullvad reconnect"
+
     tmux select-pane -t $shell_pane
 end
