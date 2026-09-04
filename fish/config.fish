2c25a151d77e6eb2fa45d7096fdc9db94865489d
Aleksei Bedniakov <x@rxtx.wtf>
2026-09-04 22:47:37 +0200

    remove thefuck

--- fish/config.fish
+++ fish/config.fish
@@ -3,18 +3,6 @@ if status is-interactive
 
     # Run ssh agent plugin
     fish_ssh_agent
-    # Defer the Python startup cost until the correction command is first used.
-    function fuck
-        if not command -q thefuck
-            echo "thefuck is not installed" >&2
-            return 127
-        end
-
-        functions --erase fuck
-        thefuck --alias | source
-        fuck $argv
-    end
-
     # Set up session logging
     #if test -z "$SCRIPTED"
     #set -gx SCRIPTED nested
